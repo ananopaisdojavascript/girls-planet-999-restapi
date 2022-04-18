02f76fe035e98edff9d690f825956cee7f2555cd
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-04-18 18:51:23 -0300

    Conexão com a base de dados

--- server.ts
+++ server.ts
@@ -1,6 +1,20 @@
 import express, { Request, Response } from 'express';
+import mysql from 'mysql';
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 3000;
+let connection;
+
+if(process.env.CLEARDB_DATABASE_URL) {
+    connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL)
+} else {
+    connection = mysql.createConnection({ 
+        host: process.env.DATABASE_HOST, 
+        user: process.env.DATABASE_USER, 
+        password: process.env.DATABASE_PASSWORD, 
+        database: process.env.DATABASE_DB 
+    });
+}
 
 app.get('/participants', (request: Request, response: Response) => {
     response.send("Hey!!!!!")
